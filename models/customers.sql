ff3232899976868f2e9baa9a9cfcfb42b6c34990
Sebastian <sjoerz@skyface.de>
2024-03-28 14:18:03 +0100

    .

--- models/customers.sql
+++ models/customers.sql
@@ -63,17 +63,17 @@ blabla as (
     select
         _airbyte_emitted_at,
         (current_timestamp at time zone 'utc')::timestamp as _airbyte_normalized_at,
-
-        cast(jsonb_extract_path_text("_airbyte_data",'key') as varchar) as "key",
-        cast(jsonb_extract_path_text("_airbyte_data",'date') as varchar) as "date",
-        cast(jsonb_extract_path_text("_airbyte_data",'new_tested') as float) as new_tested,
-        cast(jsonb_extract_path_text("_airbyte_data",'new_deceased') as float) as new_deceased,
-        cast(jsonb_extract_path_text("_airbyte_data",'total_tested') as float) as total_tested,
-        cast(jsonb_extract_path_text("_airbyte_data",'new_confirmed') as float) as new_confirmed,
-        cast(jsonb_extract_path_text("_airbyte_data",'new_recovered') as float) as new_recovered,
-        cast(jsonb_extract_path_text("_airbyte_data",'total_deceased') as float) as total_deceased,
-        cast(jsonb_extract_path_text("_airbyte_data",'total_confirmed') as float) as total_confirmed,
-        cast(jsonb_extract_path_text("_airbyte_data",'total_recovered') as float) as total_recovered
+        "_airbyte_data" as "data_raw",
+        -- cast(jsonb_extract_path_text("_airbyte_data"::json,'key') as varchar) as "key",
+        jsonb_extract_path_text("_airbyte_data"::JSONB, 'key') as "date"
+        -- cast(jsonb_extract_path_text("_airbyte_data",'new_tested') as float) as new_tested,
+        -- cast(jsonb_extract_path_text("_airbyte_data",'new_deceased') as float) as new_deceased,
+        -- cast(jsonb_extract_path_text("_airbyte_data",'total_tested') as float) as total_tested,
+        -- cast(jsonb_extract_path_text("_airbyte_data",'new_confirmed') as float) as new_confirmed,
+        -- cast(jsonb_extract_path_text("_airbyte_data",'new_recovered') as float) as new_recovered,
+        -- cast(jsonb_extract_path_text("_airbyte_data",'total_deceased') as float) as total_deceased,
+        -- cast(jsonb_extract_path_text("_airbyte_data",'total_confirmed') as float) as total_confirmed,
+        -- cast(jsonb_extract_path_text("_airbyte_data",'total_recovered') as float) as total_recovered
     from "testdb".public.stream1
 )
 
