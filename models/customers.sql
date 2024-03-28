83c99f8ce5bc64eb202ec46cd269543d9de2a9a8
Sebastian <sjoerz@skyface.de>
2024-03-28 13:24:44 +0100

    test

--- models/customers.sql
+++ models/customers.sql
@@ -52,6 +52,28 @@ final as (
 
     left join customer_orders using (customer_id)
 
-)
+),
 
-select * from final
+-- select * from final
+
+
+-- create table "postgres"."public"."covid_epidemiology"
+blabla as (
+    select
+        _airbyte_emitted_at,
+        (current_timestamp at time zone 'utc')::timestamp as _airbyte_normalized_at,
+
+        cast(jsonb_extract_path_text("_airbyte_data",'key') as varchar) as "key",
+        cast(jsonb_extract_path_text("_airbyte_data",'date') as varchar) as "date",
+        cast(jsonb_extract_path_text("_airbyte_data",'new_tested') as float) as new_tested,
+        cast(jsonb_extract_path_text("_airbyte_data",'new_deceased') as float) as new_deceased,
+        cast(jsonb_extract_path_text("_airbyte_data",'total_tested') as float) as total_tested,
+        cast(jsonb_extract_path_text("_airbyte_data",'new_confirmed') as float) as new_confirmed,
+        cast(jsonb_extract_path_text("_airbyte_data",'new_recovered') as float) as new_recovered,
+        cast(jsonb_extract_path_text("_airbyte_data",'total_deceased') as float) as total_deceased,
+        cast(jsonb_extract_path_text("_airbyte_data",'total_confirmed') as float) as total_confirmed,
+        cast(jsonb_extract_path_text("_airbyte_data",'total_recovered') as float) as total_recovered
+    from "postgres".public.stream1
+),
+
+select * from blabla
